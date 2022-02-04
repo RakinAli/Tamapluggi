8a74d539b8b5dac6c18346e8acb7f50244cf6c2e
stovatt <sarah.tovatt@gmail.com>
2022-02-04 13:39:27 +0100

    Tried to fix navigation

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
 /*Navigation bar */ 
 function Navbar() {
 	return (
 		<nav className="Navbar">
 			<ul className = "nav-link">
-				<li>Home</li>
-				<li>Study</li>
-				<li>Flashcards</li>
-				<li>Planning</li>
-				<li>Stats</li>
+				<li><Link to="/home">Home</Link></li>
+				<li><Link to="/study">Study</Link></li>
+				<li><Link to="/flashcards">Flashcards</Link></li>
+				<li><Link to="/planning">Planning</Link></li>
+				<li><Link to="/stats">Stats</Link></li>
 			</ul>
 		</nav>
 	);
