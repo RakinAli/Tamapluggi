3b43d476c6842baa7589d5f9601c027824c7b491
stovatt <sarah.tovatt@gmail.com>
2022-02-10 17:56:27 +0100

    Lite städning

--- src/components/study.js
+++ src/components/study.js
@@ -1,18 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import Statpanel from "./Statpanel";
 import SelectButtons from "./Selectbuttons";
-import TablePicture from "../Images/study_enter.png";
 
 function Study() {
 	const iconStyle = {
 		width: "50px",
 	};
-
-	const [bool, setBool] = useState(false);
-
-	function handleBoolean() {
-		setBool(true);
-	}
 
 	return (
 		<main className="flex flex-col pt-3 ">
